3d034c47d1e3d0d6178f2ba4c8574287fa10a19a
Seth Zamora Bucol <sethzamora15@gmail.com>
2023-10-15 13:26:55 +0800

    UPDATE STUDENT PI PAGE

--- Enrollment System/Properties/Settings.Designer.cs
+++ Enrollment System/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Enrollment_System.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.5.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -31,6 +31,16 @@ namespace Enrollment_System.Properties {
         public string enrollmentConnectionString {
             get {
                 return ((string)(this["enrollmentConnectionString"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=SETH\\SQLEXPRESS;Initial Catalog=enrollment;Integrated Security=True")]
+        public string enrollmentConnectionString1 {
+            get {
+                return ((string)(this["enrollmentConnectionString1"]));
             }
         }
     }
---
+++ Enrollment System/StudentPIPage.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace Enrollment_System
+{
+    partial class StudentPIPage
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtlname = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtfname = new System.Windows.Forms.TextBox();
+            this.txtmname = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtphone = new System.Windows.Forms.TextBox();
+            this.txtemail = new System.Windows.Forms.TextBox();
+            this.txtaddress = new System.Windows.Forms.TextBox();
+            this.dtpbday = new System.Windows.Forms.DateTimePicker();
+            this.save = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtgender = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtlname
+            // 
+            this.txtlname.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtlname.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtlname.Location = new System.Drawing.Point(60, 141);
+            this.txtlname.Name = "txtlname";
+            this.txtlname.Size = new System.Drawing.Size(207, 23);
+            this.txtlname.TabIndex = 38;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label2.Location = new System.Drawing.Point(57, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 17);
+            this.label2.TabIndex = 39;
+            this.label2.Text = "Last Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label1.Location = new System.Drawing.Point(306, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 17);
+            this.label1.TabIndex = 40;
+            this.label1.Text = "First Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label3.Location = new System.Drawing.Point(57, 204);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 17);
+            this.label3.TabIndex = 41;
+            this.label3.Text = "Middle Name";
+            // 
+            // txtfname
+            // 
+            this.txtfname.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtfname.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtfname.Location = new System.Drawing.Point(309, 141);
+            this.txtfname.Name = "txtfname";
+            this.txtfname.Size = new System.Drawing.Size(207, 23);
+            this.txtfname.TabIndex = 42;
+            // 
+            // txtmname
+            // 
+            this.txtmname.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtmname.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtmname.Location = new System.Drawing.Point(60, 230);
+            this.txtmname.Name = "txtmname";
+            this.txtmname.Size = new System.Drawing.Size(207, 23);
+            this.txtmname.TabIndex = 43;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label4.Location = new System.Drawing.Point(24, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(284, 31);
+            this.label4.TabIndex = 44;
+            this.label4.Text = "Personal Information";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label5.Location = new System.Drawing.Point(306, 204);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 17);
+            this.label5.TabIndex = 45;
+            this.label5.Text = "Birthdate";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label6.Location = new System.Drawing.Point(57, 370);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 17);
+            this.label6.TabIndex = 46;
+            this.label6.Text = "Email";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label7.Location = new System.Drawing.Point(57, 291);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(49, 17);
+            this.label7.TabIndex = 47;
+            this.label7.Text = "Phone";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label8.Location = new System.Drawing.Point(57, 435);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(60, 17);
+            this.label8.TabIndex = 48;
+            this.label8.Text = "Address";
+            // 
+            // txtphone
+            // 
+            this.txtphone.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtphone.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtphone.Location = new System.Drawing.Point(60, 311);
+            this.txtphone.Name = "txtphone";
+            this.txtphone.Size = new System.Drawing.Size(207, 23);
+            this.txtphone.TabIndex = 51;
+            // 
+            // txtemail
+            // 
+            this.txtemail.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtemail.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtemail.Location = new System.Drawing.Point(60, 390);
+            this.txtemail.Name = "txtemail";
+            this.txtemail.Size = new System.Drawing.Size(456, 23);
+            this.txtemail.TabIndex = 52;
+            // 
+            // txtaddress
+            // 
+            this.txtaddress.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtaddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtaddress.Location = new System.Drawing.Point(60, 455);
+            this.txtaddress.Name = "txtaddress";
+            this.txtaddress.Size = new System.Drawing.Size(456, 23);
+            this.txtaddress.TabIndex = 54;
+            // 
+            // dtpbday
+            // 
+            this.dtpbday.Location = new System.Drawing.Point(309, 233);
+            this.dtpbday.Name = "dtpbday";
+            this.dtpbday.Size = new System.Drawing.Size(207, 20);
+            this.dtpbday.TabIndex = 55;
+            // 
+            // save
+            // 
+            this.save.BackColor = System.Drawing.Color.SteelBlue;
+            this.save.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.save.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.save.FlatAppearance.BorderSize = 0;
+            this.save.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.save.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.save.Location = new System.Drawing.Point(60, 512);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(73, 37);
+            this.save.TabIndex = 56;
+            this.save.Text = "Save";
+            this.save.UseVisualStyleBackColor = false;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label9.Location = new System.Drawing.Point(306, 291);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(56, 17);
+            this.label9.TabIndex = 57;
+            this.label9.Text = "Gender";
+            // 
+            // txtgender
+            // 
+            this.txtgender.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtgender.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtgender.Location = new System.Drawing.Point(309, 311);
+            this.txtgender.Name = "txtgender";
+            this.txtgender.Size = new System.Drawing.Size(207, 23);
+            this.txtgender.TabIndex = 58;
+            // 
+            // StudentPIPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.txtgender);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.dtpbday);
+            this.Controls.Add(this.txtaddress);
+            this.Controls.Add(this.txtemail);
+            this.Controls.Add(this.txtphone);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtmname);
+            this.Controls.Add(this.txtfname);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtlname);
+            this.Name = "StudentPIPage";
+            this.Size = new System.Drawing.Size(966, 723);
+            this.Load += new System.EventHandler(this.StudentPIPage_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtlname;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtfname;
+        private System.Windows.Forms.TextBox txtmname;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtphone;
+        private System.Windows.Forms.TextBox txtemail;
+        private System.Windows.Forms.TextBox txtaddress;
+        private System.Windows.Forms.DateTimePicker dtpbday;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtgender;
+    }
+}
